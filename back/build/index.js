712c0ef652d5288c554f29096e36765f8211496d
OGabrielPereira <ogabrielpereira@pm.me>
2022-06-04 10:33:05 -0300

    back

---
+++ back/build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.get("/", (res, req) => {
+    req.send("Hello");
+});
+app.listen(PORT, () => {
+    console.log(PORT);
+});
