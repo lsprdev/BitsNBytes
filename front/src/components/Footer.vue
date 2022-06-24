7263637d3f0604417a36ead4903dfe5e2c14abc4
gustavokubiack <guukubiack@gmail.com>
2022-06-24 01:08:23 -0300

    adding footer

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -25,55 +25,89 @@
           "
         >
           <img class="h-fit w-12" src="../assets/logo3.png" />
-          <span class="text-white font-semibold text-xl tracking-tight px-2"
-            >IF ACOLHE</span
-          >
         </a>
-        <p class="mt-6 text-l text-justify text-white">
+        <p class="mt-4 text-l text-justify text-white">
           O IF Acolhe é um projeto realizado pelos estudantes da 3info3 que tem
           como objetivo controlar os animais que circulam no Campus.
         </p>
-
-        <div class="flex justify-center mt-4 lg:mt-2">
-          <br />
-          <p class="text-white">Rede social</p>
-          <a class="ml-3" href="https://www.instagram.com/ifacolhe/">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-6 h-6 text-pink-400"
-              viewBox="0 0 24 24"
-            >
-              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-              <path
-                d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
-              ></path>
-            </svg>
-          </a>
-        </div>
       </div>
       <div class="justify-between w-full mt-4 text-center lg:flex">
         <div class="w-full px-4 lg:w-1/3 md:w-1/2">
           <h2 class="text-white mb-6 font-semibold text-xl tracking-tight px-2">
             LINKS ÚTEIS
           </h2>
-          <ul class="mb-6 space-y-2 text-l list-none">
+          <ul class="mb-6 space-y-4 text-l list-none">
             <li>
-              <a class="text-white" href="/">Home</a>
+              <a class=" py-2 px-2 font-semibold text-white rounded hover:bg-white hover:text-[#15393C] transition duration-300 mx-2" href="/">Home</a>
             </li>
             <li>
-              <a class="text-white" href="/adote">Adote</a>
+              <a class="py-2 px-2 font-semibold text-white rounded hover:bg-white hover:text-[#15393C] transition duration-300 mx-2" href="/adote">Adote</a>
             </li>
             <li>
-              <a class="text-white" href="/apoie">Apoie</a>
+              <a class="py-2 px-2 font-semibold text-white rounded hover:bg-white hover:text-[#15393C] transition duration-300 mx-2" href="/apoie">Apoie</a>
             </li>
             <li>
-              <a class="text-white" href="/">Galeria</a>
+              <a class="py-2 px-2 font-semibold text-white rounded hover:bg-white hover:text-[#15393C] transition duration-300 mx-2" href="/">Galeria</a>
             </li>
           </ul>
+        </div>
+        <div class="w-full px-4 lg:w-1/3 md:w-1/2">
+          <h2 class="text-white mb-6 font-semibold text-xl tracking-tight px-2">
+            CONTATO
+          </h2>
+          <ul class="mb-6 space-y-2 text-l list-none text-justify">
+            <li>
+              <p class="text-white">
+                Endereço: BR-280 - Colégio Agrícola, Araquari - SC, 89245-000
+              </p>
+            </li>
+            <li>
+              <p class="text-white" href="/adote">
+                Telefone: &#40;47&#41; 3803-7200
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="w-full px-4 lg:w-1/3 md:w-1/2">
+          <h2 class="text-white mb-6 font-semibold text-xl tracking-tight px-2">
+            REDES SOCIAIS
+          </h2>
+          <div class="flex justify-center mt-4 lg:mt-2">
+            <br />
+            <a
+              class="ml-3 text-white"
+              href="https://www.instagram.com/ifacolhe/"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6 text-pink-400"
+                viewBox="0 0 24 24"
+              >
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path
+                  d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
+                ></path>
+              </svg>
+            </a>
+            <a href="https://www.facebook.com/ifacolhe">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6 text-blue-600"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+                ></path>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- front/src/main.js
+++ front/src/main.js
@@ -3,6 +3,7 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+
 // Taiwlwind
 import "./index.css";
 
