9d47f74396dd564bc68069b071dbfd3c1e1621f4
OGabrielPereira <ogabrielpereira@pm.me>
2022-06-24 13:37:12 -0300

    GaleriaCards created

--- front/src/views/Galeria.vue
+++ front/src/views/Galeria.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        
+        <GaleriaCards />
     </div>
 </template>
+
+<script>
+import GaleriaCards from "@/components/GaleriaCards.vue";
+
+export default {
+    components: {
+        GaleriaCards,
+    },
+}
+
+
+
+</script>
